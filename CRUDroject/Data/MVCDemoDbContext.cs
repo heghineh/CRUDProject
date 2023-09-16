51c5d1e674c519d207eb59ceb26af4a9326ec80c
ASUS <heghine.aghajanyan.gk@gmail.com>
2023-09-16 18:50:22 +0400

    Добавьте файлы проекта.

---
+++ CRUDroject/Data/MVCDemoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CRUDroject.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDroject.Data
+{
+    public class MVCDemoDbContext : DbContext 
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //We should create properties which are used
+        //to access the tables that EntityFrameworkCore will create in our DB
+
+        //Shortcut prop + tab + tab 
+        //Now let's create Domain models in the Models folder
+        public DbSet<Employee> Employees { get; set; }
+        //Now we are going to inject this inside the program.sc
+    }
+}
---
+++ CRUDroject/Models/AddEmloyeeViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRUDroject.Models
+{
+    public class AddEmloyeeViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
